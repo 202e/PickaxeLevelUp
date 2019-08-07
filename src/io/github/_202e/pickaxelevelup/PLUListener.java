44100a5cd1a82e73f2b507a7c399bfedc583b04d
202e <37431702+202e@users.noreply.github.com>
2019-08-07 03:01:23 -0700

    move list of pickaxe materials to main plugin class for accessibility

--- src/io/github/_202e/pickaxelevelup/PLUListener.java
+++ src/io/github/_202e/pickaxelevelup/PLUListener.java
@@ -1,18 +1,12 @@
 package io.github._202e.pickaxelevelup;
 
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
-import java.util.Arrays;
-import java.util.List;
-
 class PLUListener implements Listener {
-    private static final List<Material> picks = Arrays.asList(Material.WOOD_PICKAXE, Material.STONE_PICKAXE,
-            Material.IRON_PICKAXE, Material.GOLD_PICKAXE, Material.DIAMOND_PICKAXE);
     private final PickaxeLevelUp plugin;
 
     PLUListener(PickaxeLevelUp plugin) {
@@ -23,7 +17,7 @@ class PLUListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent pie) {
         if (plugin.rightClickEnabled && pie.getHand().equals(EquipmentSlot.HAND) &&
                 (pie.getAction() == Action.RIGHT_CLICK_AIR || pie.getAction() == Action.RIGHT_CLICK_BLOCK) &&
-                picks.contains(pie.getMaterial())) {
+                PickaxeLevelUp.picks.contains(pie.getMaterial())) {
             pie.getPlayer().performCommand(plugin.rightClickCommand);
         }
     }
--- src/io/github/_202e/pickaxelevelup/PickaxeLevelUp.java
+++ src/io/github/_202e/pickaxelevelup/PickaxeLevelUp.java
@@ -1,8 +1,14 @@
 package io.github._202e.pickaxelevelup;
 
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PickaxeLevelUp extends JavaPlugin {
+    public static final List<Material> picks = Arrays.asList(Material.WOOD_PICKAXE, Material.STONE_PICKAXE,
+            Material.IRON_PICKAXE, Material.GOLD_PICKAXE, Material.DIAMOND_PICKAXE);
     String rightClickCommand;
     boolean rightClickEnabled;
 
