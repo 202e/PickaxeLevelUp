0e5849faed1f25edcc6f6f53a15ab65b00c892cb
202e <37431702+202e@users.noreply.github.com>
2019-08-07 03:01:09 -0700

    add support for a config to determine if command will run when pickaxe is right clicked, and which command will be run

--- src/io/github/_202e/pickaxelevelup/PickaxeLevelUp.java
+++ src/io/github/_202e/pickaxelevelup/PickaxeLevelUp.java
@@ -3,11 +3,16 @@ package io.github._202e.pickaxelevelup;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PickaxeLevelUp extends JavaPlugin {
-    String rightClickCommand = "";
-    boolean rightClickEnabled = true;
+    String rightClickCommand;
+    boolean rightClickEnabled;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        this.rightClickEnabled = getConfig().getBoolean("rightclickenabled");
+        this.rightClickCommand = getConfig().getString("rightclickcommand");
+
         getServer().getPluginManager().registerEvents(new PLUListener(this), this);
     }
 }
